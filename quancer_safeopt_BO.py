7893898d80d590005c7110616e528293fb57eb46
Dimitris <dimgkoutz@gmail.com>
2024-10-11 12:25:52 +0300

    load_agent_data defined

--- quancer_safeopt_BO.py
+++ quancer_safeopt_BO.py
@@ -15,6 +15,13 @@ def retrieve_data(target_uri, modelName, gain_arg, std_args):
     subprocess.call(sys_get, shell=True)
     shutil.copyfile('servoPDF.mat', 'servoPDF.mat')
     
+def load_agent_data(filename):
+    data = loadmat(filename)
+    rt_t = data['rt_t'].flatten()
+    rt_theta = data['rt_theta'].flatten()
+    theta_d = data['rt_theta_d'].flatten()
+    return rt_t, rt_theta, theta_d
+
     
 def compute_reward(theta_d, rt_theta1, rt_theta2, rt_t1, rt_t2):
     # Overshoot error
@@ -53,19 +60,19 @@ subprocess.call(sys2dl, shell=True)
 # Initial safepoint values.
 
 
-kp1 = 0.11
-kd1 = 0.05
+kp1_0 = 0.11
+kd1_0 = 0.05
 
-kp2 = 0.09
-kd2 = 0.05
+kp2_0 = 0.09
+kd2_0 = 0.05
 
 
 #delay difference between the two agents
 td1 = 0.045
 
 # create gain arguments
-gain_arg1 = f' -Kp {kp1:.4f} -Kd {kd1:.4f}'
-gain_arg2 = f' -Kp {kp2:.4f} -Kd {kd2:.4f}'
+gain_arg1 = f' -Kp {kp1_0:.4f} -Kd {kd1_0:.4f}'
+gain_arg2 = f' -Kp {kp2_0:.4f} -Kd {kd2_0:.4f}'
 
 print(f'Initial gain arguments for Agent 1: {gain_arg1}')
 print(f'Initial gain arguments for Agent 2: {gain_arg2}')
@@ -84,57 +91,39 @@ subprocess.call(sys2run, shell=True)
 # Possibly by checking the port for incoming data
 time.sleep(6)
 
-# Retrieve data from board
-sys1get = f'quarc_run -u -t {target_uri_1} {modelName}.rt-linux_rt_armv7{gain_arg1}{std_args}'
-sys2get = f'quarc_run -u -t {target_uri_2} {modelName}.rt-linux_rt_armv7{gain_arg2}{std_args}'
-
 
 #retrieve data from Agent 1
-# retrieve_data(target_uri_1, modelName, gain_arg1, std_args)
-# Retrieve data from Agent 1
-subprocess.call(sys1get, shell=True)
-
-# store data in a new file
-shutil.copyfile('servoPDF.mat', 'servoPDF-1.mat')
+retrieve_data(target_uri_1, modelName, gain_arg1, std_args)
 
 #retrieve data from Agent 2
-subprocess.call(sys2get, shell=True)
-#store data in a new file
-shutil.copyfile('servoPDF.mat', 'servoPDF-2.mat')
+retrieve_data(target_uri_2, modelName, gain_arg2, std_args)
 
-#load data for agent 1
-data1 = loadmat('servoPDF-1.mat')
-rt_t1 = data1['rt_t'].flatten()
-rt_theta1 = data1['rt_theta'].flatten()
+# Load data for agent 1
+rt_t1, rt_theta1,theta_d = load_agent_data('servoPDF-1.mat')
 
 #load data for agent 2
-data2 = loadmat('servoPDF-2.mat')
-rt_t2 = data2['rt_t'].flatten()
-rt_theta2 = data2['rt_theta'].flatten()
+rt_t2, rt_theta2, _ = load_agent_data('servoPDF-2.mat')
 
-# load desired theta
-theta_d = data1['rt_theta_d'].flatten()
-
+#compute initial safe reward
 reward_0, _ , _ = compute_reward(theta_d,rt_theta1,rt_theta2,rt_t1,rt_t2)
 
 print(f'Initial reward: {reward_0}')
 exit(0)
-dsadasd
 
 # =================== Bayesian Optimization ===================
 
 # Define the Agent class for Bayesian Optimization
 class Agent:
-    def __init__(self, id, bounds, safe_point):
+    def __init__(self, id, bounds, safe_point,initial_reward):
         self.id = id
         self.bounds = [bounds]
         self.safe_point = safe_point
 
-        self.z0 = np.array([[safe_point]])
-        self.y0 = np.array([[0.0]])  # Initial observation (reward)
+        self.x0 = np.array([[safe_point]])
+        self.y0 = np.array([[initial_reward]]) 
 
         self.kernel = GPy.kern.RBF(1)
-        self.gp = GPy.models.GPRegression(self.z0, self.y0, self.kernel, noise_var=0.05**2)
+        self.gp = GPy.models.GPRegression(self.x0, self.y0, self.kernel, noise_var=0.05**2)
 
         self.parameter_set = safeopt.linearly_spaced_combinations(self.bounds, 100)
         self.opt = safeopt.SafeOpt(self.gp, self.parameter_set, fmin=0.0)
@@ -143,22 +132,19 @@ class Agent:
         self.rewards = [0.0]
 
     def optimize(self):
-        z_next = self.opt.optimize()
-        return z_next
+        x_next = self.opt.optimize()
+        return x_next
 
-    def update(self, z_next, y_meas):
-        self.opt.add_new_data_point(z_next, y_meas)
-        self.kp_values.append(z_next[0])
+    def update(self, x_next, y_meas):
+        self.opt.add_new_data_point(x_next, y_meas)
+        self.kp_values.append(x_next[0])
         self.rewards.append(y_meas)
 
-# Initialize agents with safe initial Kp values
-kp_bounds = (0.1, 10.0)
-safe_kp1 = 4.0  # Same as initial kp1
-safe_kp2 = 4.0  # Same as initial kp2
-Kd = 0.05       # Keep Kd constant as per your instructions
+# Kp bounds
+kp_bounds = (0.01, 400)
 
-agent1 = Agent(1, kp_bounds, safe_kp1)
-agent2 = Agent(2, kp_bounds, safe_kp2)
+agent1 = Agent(1, kp_bounds, kp1_0,reward_0)
+agent2 = Agent(2, kp_bounds, kp2_0,reward_0)
 
 # Define function to run the experiment and compute the error
 def run_experiment(kp1, kp2):
@@ -166,9 +152,11 @@ def run_experiment(kp1, kp2):
     kp1_value = kp1[0]  # Extract value from array
     kp2_value = kp2[0]
 
-    gain_arg1 = f' -Kp {kp1_value:.4f} -Kd {Kd:.4f}'
-    gain_arg2 = f' -Kp {kp2_value:.4f} -Kd {Kd:.4f}'
+    # set gain arguments
+    gain_arg1 = f' -Kp {kp1_value:.4f} -Kd {kd1_0:.4f}'
+    gain_arg2 = f' -Kp {kp2_value:.4f} -Kd {kd2_0:.4f}'
 
+    # set system commands
     sys1run = f'quarc_run -l -t {target_uri_1} {modelName}.rt-linux_rt_armv7{gain_arg1} {std_args}'
     sys2run = f'quarc_run -l -t {target_uri_2} {modelName}.rt-linux_rt_armv7{gain_arg2} {std_args}'
 
@@ -176,6 +164,7 @@ def run_experiment(kp1, kp2):
     subprocess.call(sys1run, shell=True)
     subprocess.call(sys2run, shell=True)
 
+    # await experiment completion
     time.sleep(6)
 
     # Retrieve data
@@ -189,6 +178,7 @@ def run_experiment(kp1, kp2):
     shutil.copyfile('servoPDF.mat', 'servoPDF-2.mat')
 
     # Load data
+    
     data1 = loadmat('servoPDF-1.mat')
     rt_t1 = data1['rt_t'].flatten()
     rt_theta1 = data1['rt_theta'].flatten()
@@ -209,7 +199,7 @@ def run_experiment(kp1, kp2):
 
     return integral_error1, integral_error2
 
-# Run Bayesian Optimization
+
 N = 10  # Number of iterations
 
 for iteration in range(N):
